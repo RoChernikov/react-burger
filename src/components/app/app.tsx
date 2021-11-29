96b8e7aab7f69497bf40199b0a9ad83a3fc94e60
Ro Chernikov <imrhubris@gmail.com>
2021-11-29 16:26:11 +0300

    feat:header has been added

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppHeader from '../app-header/app-header';
+import styles from './app.module.css';
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+    </>
+  );
+}
+
+export default App;

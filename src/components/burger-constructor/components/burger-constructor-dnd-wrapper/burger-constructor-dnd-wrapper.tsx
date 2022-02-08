027a7946748b3e65b6d888b16a3c855e4f532a4f
Ro Chernikov <imrhubris@gmail.com>
2022-02-08 16:38:46 +0300

    feat: typescript migration: loader, modal, order-details

--- src/components/burger-constructor/components/burger-constructor-dnd-wrapper/burger-constructor-dnd-wrapper.tsx
+++ src/components/burger-constructor/components/burger-constructor-dnd-wrapper/burger-constructor-dnd-wrapper.tsx
@@ -3,7 +3,7 @@ import { useDrop } from 'react-dnd';
 import { useAppDispatch } from '../../../../services/hooks';
 import BurgerConstructor from '../../burger-constructor';
 import { burgerConstructorSlice } from '../../../../services/slices/constructor';
-import { TDropIngredient } from '../../../../utils/types';
+import { TDestructIngredient } from '../../../../utils/types';
 import { IBurgerConstructor } from '../../../../utils/interfaces';
 //--------------------------------------------------------------------------------
 
@@ -12,7 +12,7 @@ const BurgerConstructorDndWrapper: FC<IBurgerConstructor> = ({ openModal }) => {
   const { dropIngredient } = burgerConstructorSlice.actions;
   const [{ isHover }, dropTarget] = useDrop({
     accept: 'ingredient',
-    drop({ ingredient }: TDropIngredient) {
+    drop({ ingredient }: TDestructIngredient) {
       dispatch(dropIngredient(ingredient));
     },
     collect: monitor => ({

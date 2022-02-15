367060f6848b4e81c176bcfa525431d32745af23
Ro Chernikov <imrhubris@gmail.com>
2022-02-15 09:37:44 +0300

    feat: forgot password and reset password pages have been added

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,6 +5,8 @@ import LoginPage from '../../pages/login/login';
 import RegisterPage from '../../pages/register/register';
 import ProfilePage from '../../pages/profile/profile';
 import FeedPage from '../../pages/feed/feed';
+import ForgotPasswordPage from '../../pages/forgot-password/forgot-password';
+import ResetPasswordPage from '../../pages/reset-password/reset-password';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 //--------------------------------------------------------------------------------
 
@@ -22,6 +24,12 @@ const App: FC = () => {
           </Route>
           <Route path="/register" exact>
             <RegisterPage />
+          </Route>
+          <Route path="/forgot-password" exact>
+            <ForgotPasswordPage />
+          </Route>
+          <Route path="/reset-password" exact>
+            <ResetPasswordPage />
           </Route>
           <Route path="/profile" exact>
             <ProfilePage />
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,3 +1,4 @@
+import styles from './login.module.css';
 import React, { FC, useState } from 'react';
 import {
   Input,
@@ -18,35 +19,37 @@ const LoginPage: FC = () => {
   const [password, setPassword] = useState('');
 
   return (
-    <Form onSubmit={handleSubmit} title="Вход">
-      <InputWrapper>
-        <Input
-          name="email"
-          value={email}
-          type="text"
-          placeholder="E-mail"
-          error={false}
-          errorText="Ошибка"
-          onChange={e => setEmail(e.target.value)}
-        />
-      </InputWrapper>
-      <InputWrapper>
-        <PasswordInput
-          name="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </InputWrapper>
-      <Submit>Войти</Submit>
-      <FormHint link="/register" caption="Зарегистрироваться">
-        Вы - новый пользователь?
-      </FormHint>
-      <div className="mt-4">
-        <FormHint link="/forgot-password" caption="Восстановить пароль">
-          Забыли пароль?
+    <main className={styles.main}>
+      <Form onSubmit={handleSubmit} title="Вход">
+        <InputWrapper>
+          <Input
+            name="email"
+            value={email}
+            type="text"
+            placeholder="E-mail"
+            error={false}
+            errorText="Ошибка"
+            onChange={e => setEmail(e.target.value)}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <PasswordInput
+            name="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </InputWrapper>
+        <Submit>Войти</Submit>
+        <FormHint link="/register" caption="Зарегистрироваться">
+          Вы - новый пользователь?
         </FormHint>
-      </div>
-    </Form>
+        <div className="mt-4">
+          <FormHint link="/forgot-password" caption="Восстановить пароль">
+            Забыли пароль?
+          </FormHint>
+        </div>
+      </Form>
+    </main>
   );
 };
 

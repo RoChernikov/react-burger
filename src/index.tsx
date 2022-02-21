1e38fc392b8dce09c3362559cc4862e63dc7f4fc
Ro Chernikov <imrhubris@gmail.com>
2022-02-21 18:17:22 +0300

    feat: feed page has been added

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import state from './services/store';
 //--------------------------------------------------------------------------------
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={state}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

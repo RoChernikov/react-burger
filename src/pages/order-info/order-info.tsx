5cb1fa41a3db72c251d6c1cfb402e87e0f581dd6
Ro Chernikov <imrhubris@gmail.com>
2022-03-10 20:18:59 +0300

    feat: order info page is done

--- src/pages/order-info/order-info.tsx
+++ src/pages/order-info/order-info.tsx
@@ -1,13 +1,37 @@
 import styles from './order-info.module.css';
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { wsInit, wsClose } from '../../services/slices/ws-orders';
+import { useAppDispatch, useAppSelector } from '../../services/hooks';
 import OrderInfo from '../../components/order-info/order-info';
+import Loader from '../../components/loader/loader';
+import Message from '../../components/message/message';
 //--------------------------------------------------------------------------------
 
 const OrderInfoPage: FC = () => {
-  return (
-    <section className={styles.section}>
-      <OrderInfo />
-    </section>
+  const { wsRequest, wsFailed, wsOpen, orders } = useAppSelector(
+    state => state.wsOrders
+  );
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(wsInit());
+
+    return () => {
+      dispatch(wsClose());
+    };
+  }, [dispatch]);
+
+  return orders.length === 0 && !wsFailed ? (
+    <Loader />
+  ) : (
+    <>
+      {wsFailed ? (
+        <Message>Не удалось загрузить данные!</Message>
+      ) : (
+        <section className={styles.section}>
+          <OrderInfo />
+        </section>
+      )}
+    </>
   );
 };
 

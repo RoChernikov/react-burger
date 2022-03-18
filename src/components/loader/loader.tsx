e6be1cd02354a3a478986367bf96282fe40bc5e7
Ro Chernikov <imrhubris@gmail.com>
2022-03-19 01:21:49 +0300

    ref: css-> scss

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,4 +1,4 @@
-import styles from './loader.module.css';
+import styles from './loader.module.scss';
 import React, { FC } from 'react';
 //--------------------------------------------------------------------------------
 

216283a2fca5ffd6039743a0f60eac301edb7bcf
Ro Chernikov <imrhubris@gmail.com>
2022-05-06 15:40:48 +0300

    AppThunk type and package update

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,15 +1,16 @@
-import thunk, { ThunkAction } from 'redux-thunk';
-import { Action, ActionCreator } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './slices';
-import { socketMiddleware } from '../middleware/socket-middleware';
-import { WS_ORDER_ACTION_TYPES } from './slices/ws-orders';
+import thunk, {ThunkAction} from 'redux-thunk';
+import {ActionCreator, AnyAction} from 'redux';
+import {configureStore} from '@reduxjs/toolkit';
+import {rootReducer} from './slices';
+import {socketMiddleware} from '../middleware/socket-middleware';
+import {WS_ORDER_ACTION_TYPES} from './slices/ws-orders';
 //--------------------------------------------------------------------------------
 
 export type RootState = ReturnType<typeof state.getState>;
-export type AppThunk<TReturn = void> = ActionCreator<
-  ThunkAction<TReturn, RootState, unknown, Action>
+export type AppThunk<ReturnType = void> = ActionCreator<
+  ThunkAction<ReturnType, RootState, any, AnyAction>
 >;
+
 export type AppDispatch = typeof state.dispatch;
 
 const state = configureStore({
